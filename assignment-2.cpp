15d6168778491b3a1661f79ab4699e99d5484acd
Hamish Hare <22hvh1@queensu.ca>
2024-02-23 11:47:40 +0000

    Replaced instances of std::vector with vector by including a 'using' statement at the top

--- assignment-2.cpp
+++ assignment-2.cpp
@@ -13,7 +13,7 @@
 
 #include<vector>
 #include<string>
-using std::string;
+using std::string; using std::vector;
 #include<cstring> // TEMP
 //#include<sstream>
 
@@ -22,7 +22,7 @@ typedef std::tuple<double, int, string> course_tuple;
 
 // Helper functions
 // .. Read in all the lines from a file and return as a vector of strings
-std::vector<string> read_lines_from_file(string file_name)
+vector<string> read_lines_from_file(string file_name)
 {
   // Open file
   // (Maybe open the file elsewhere and just pass the filestream by
@@ -34,7 +34,7 @@ std::vector<string> read_lines_from_file(string file_name)
   
   // Make each line (as a string) into a vector entry
   string current_line;
-  std::vector<string> all_lines;
+  vector<string> all_lines;
   while (std::getline(course_stream, current_line))
   {
     all_lines.push_back(current_line);
@@ -45,15 +45,15 @@ std::vector<string> read_lines_from_file(string file_name)
 }
 
 // Split each course entry into component parts
-std::vector<course_tuple> get_course_info(std::vector<string> &all_lines)
+vector<course_tuple> get_course_info(vector<string> &all_lines)
 {
-  std::vector<course_tuple> all_courses_info;
+  vector<course_tuple> all_courses_info;
   // Iterate over all the lines in the file and add a course detail
   // tuple to the all_courses_info vector:
   double grade;
   int course_code;
   string course_title;
-  std::vector<string>::iterator line_entry;
+  vector<string>::iterator line_entry;
   for(line_entry=all_lines.begin(); line_entry<all_lines.end(); ++line_entry)
   {
     // TODO:
@@ -77,14 +77,14 @@ std::vector<course_tuple> get_course_info(std::vector<string> &all_lines)
 //    just passing the vector of tuples and extracting the grade
 //    I am extracting them here so that only the array of numbers
 //    is passed in to the mean and std functions.)
-std::vector<double> extract_grades(std::vector<course_tuple>& course_info)
+vector<double> extract_grades(vector<course_tuple>& course_info)
 {
   // TODO:
   // - Validation? Maybe check for empty vector first?
-  std::vector<double> all_grades;
+  vector<double> all_grades;
   // Iterate over all course entries to extract the grade
   double grade;
-  std::vector<course_tuple>::iterator course_entry;
+  vector<course_tuple>::iterator course_entry;
   for(course_entry=course_info.begin(); course_entry<course_info.end();
       ++course_entry)
   {
@@ -115,11 +115,11 @@ int main()
   //std::ifstream course_stream(data_file);
 
   // Read in all the lines from the file
-  std::vector<string> all_file_lines = read_lines_from_file(data_file_name);
+  vector<string> all_file_lines = read_lines_from_file(data_file_name);
   size_t num_courses{all_file_lines.size()};
 
   // Split each line into grade, course code, and course title
-  std::vector<course_tuple> all_course_details = get_course_info(all_file_lines);
+  vector<course_tuple> all_course_details = get_course_info(all_file_lines);
 
   // Close file
   //course_stream.close();
@@ -127,7 +127,7 @@ int main()
   // Print out list of all courses (correctly formatted)
 
   // TEST: Extract all grades
-  std::vector<double> all_grades = extract_grades(all_course_details);
+  vector<double> all_grades = extract_grades(all_course_details);
 
   // Print number of courses requested
 
