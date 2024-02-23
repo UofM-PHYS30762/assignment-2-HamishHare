4daa1a3135eb8616771271e6d5a77bd4e9b9470e
Hamish Hare <22hvh1@queensu.ca>
2024-02-23 00:02:13 +0000

    A first test of reading all the lines from a file into a vector of strings. Currently hard-coded file name and using basic c-style strings. The next step is to make this flexible to a user inputted file name.

--- assignment-2.cpp
+++ assignment-2.cpp
@@ -2,31 +2,61 @@
 // Assignment 2
 
 // Program to compute mean, standard deviation and standard
-// error of the a set of courses. Data is read from file
+// error of a chosen set of courses. Data is read from file.
 
-// Skeleton code - assessed components are missing
+// Hamish Hare -- adapted from skeleton code.
 
 #include<iostream>
 #include<iomanip>
 #include<fstream>
 #include<cmath>
 
-// Functions to compute mean, standard deviation or for other tasks.
+#include<vector>
+#include<string>
+#include<cstring> // TEMP
+//#include<sstream>
+
+// Helper functions
+// .. Read in all the lines from a file and return as a vector of strings
+//std::vector<std::string> read_lines_from_file(char& filename)
+int read_lines_from_file()
+{
+  char data_file[100];
+  std::strcpy(data_file, "courselist.dat");
+
+  // Open file (you must check if successful)
+  std::ifstream course_stream(data_file);
+
+  std::string current_line;
+  std::vector<std::string> all_lines;
+  while (std::getline(course_stream, current_line))
+  {
+    all_lines.push_back(current_line);
+  }
+  //return all_lines;
+  return 0;
+}
+// .. Compute the mean and standard deviation of given scores.
+// TODO
 
 // Main function
 
 int main()
 {
   // Define variables
-  char data_file[100];
+  //char data_file[100];
   // int number_courses;
 
   // Ask user to enter filename
-  std::cout<<"Enter data filename: ";
-  std::cin>>data_file;
+  //std::cout<<"Enter data filename: ";
+  //std::cin>>data_file;
 
   // Open file (you must check if successful)
-  std::fstream course_stream(data_file);
+  //std::fstream course_stream(data_file);
+  //std::ifstream course_stream(data_file);
+
+  // TEST: read lines from file
+  int test = read_lines_from_file();
 
   // Allocate memory for data 
 
@@ -38,7 +68,7 @@ int main()
   //  }
 
   // Close file
-  course_stream.close();
+  //course_stream.close();
 
   // Print number of courses requested
 
