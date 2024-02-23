b972745f4063d788ac4b7df283c623b8576296d9
Hamish Hare <22hvh1@queensu.ca>
2024-02-23 01:49:44 +0000

    Added a function to break a single line course entry into its component parts and return this as a tuple of grade, course code, and course title. Again, no validation yet.

--- assignment-2.cpp
+++ assignment-2.cpp
@@ -17,6 +17,9 @@ using std::string;
 #include<cstring> // TEMP
 //#include<sstream>
 
+// Constants
+typedef std::tuple<double, int, string> course_tuple;
+
 // Helper functions
 // .. Read in all the lines from a file and return as a vector of strings
 std::vector<string> read_lines_from_file(string file_name)
@@ -25,7 +28,9 @@ std::vector<string> read_lines_from_file(string file_name)
   // (Maybe open the file elsewhere and just pass the filestream by
   //  reference into this function, rather than the file name)
   std::ifstream course_stream(file_name);
-  // TODO: Validation! e.g. empty files, etc.
+  // TODO: Validation!
+  // .. check for empty files
+  // .. only include valid lines in return
   
   // Make each line (as a string) into a vector entry
   string current_line;
@@ -34,8 +39,21 @@ std::vector<string> read_lines_from_file(string file_name)
   {
     all_lines.push_back(current_line);
   }
+  // Close file
   course_stream.close();
   return all_lines;
+}
+
+// Split each course entry into component parts
+course_tuple get_course_info(string line_entry)
+{
+  // TODO:
+  // - Validation
+  // - Improve flexibility, rather than hardcoded splitting
+  double grade{std::stod(line_entry.substr(0, 4))};
+  int course_code{std::stoi(line_entry.substr(5, 5))};
+  string course_title{line_entry.substr(13)};
+  return std::make_tuple(grade, course_code, course_title);
 }
 
 // .. Compute the mean and standard deviation of given scores.
@@ -44,12 +62,9 @@ std::vector<string> read_lines_from_file(string file_name)
 // Main function
 int main()
 {
-  // Define variables
+  // Get filename from user
   string data_file_name;
   //string data_file_name{"courselist.dat"};
-  size_t num_courses;
-
-  // Ask user to enter filename
   std::cout<<"Enter data filename: ";
   // TODO: Validation
   std::getline(std::cin, data_file_name);
@@ -59,16 +74,11 @@ int main()
 
   // Read in all the lines from the file
   std::vector<string> all_file_lines = read_lines_from_file(data_file_name);
-  num_courses = all_file_lines.size();
+  size_t num_courses{all_file_lines.size()};
 
-  // Allocate memory for data 
-
-  // Read data from file
-  //e.g. 
-  //for(int i{};i<Ndata;i++) 
-  //  {
-  //
-  //  }
+  // Split each line into grade, course code, and course title
+  course_tuple course_details = get_course_info(all_file_lines[0]);
+  //std::vector<course_tuple> course_details = 
 
   // Close file
   //course_stream.close();
